6f82370e2db1c6061c376b305d0ced4ad42ba73d
davodhiambo <jesseomolo@yahoo.com>
2024-12-24 01:58:07 +0300

    templates: add email template.

--- models/client.go
+++ models/client.go
@@ -9,3 +9,13 @@ var Clients = []Client{
 	{Name: "Jesse Yahoo", Email: "jesseomolo@yahoo.com"},
 	{Name: "Jesse Mooka", Email: "jessemooka@gmail.com"},
 }
+
+const EmailTemplate = `
+Hi {{.Name}},
+I hope this email finds you well.
+I'm excited to share that you're eligible for a limited time offer on our website building offer. Order your website and get 50%\ off on your purchase. Take advantage of this offer before it's expiration on 31st January 2025.
+
+Happy Holidays and a Prosperous New Year!
+David Jesse,
+
+`
